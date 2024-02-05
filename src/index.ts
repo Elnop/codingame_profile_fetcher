dc80e36621c294fb0b701f9c8fa1ce569438d813
Elnop <elnop@dev.fr>
2024-02-05 07:17:34 +0100

    add index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { get_codingamer_by_url } from "./get_codingamer_by_url/get_codingamer_by_url";
 import { Codingamer } from './Codingamer/Codingamer';
+import { Codingame_API_Exeption } from './Codingamer_Exeption';
 
-export { get_codingamer_by_url, Codingamer }
+export {
+    Codingamer,
+    get_codingamer_by_url,
+    Codingame_API_Exeption
+}
