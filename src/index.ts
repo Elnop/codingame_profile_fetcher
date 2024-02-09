6d63adce3b6322586dae7d31ffe88cb6dc42362a
Elnop <elnop@dev.fr>
2024-02-09 09:29:01 +0100

    adding definitions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,19 @@ export {
     Codingame_API_Exeption,
     get_image_url
 }
+
+import { T_Quest_Certification } from './types/T_Quest_Certification';
+import { T_Certification, T_Programming_Language } from './types/T_Programing_language';
+import { T_Topic_Skill } from './types/T_Topic_Skill';
+import { T_Xp_Threshold } from './types/T_Xp_Threshold';
+import { Codingame_API_Exeption_Function, Codingame_API_Exeption_Type } from './Codingamer_Exeption';
+
+export  {
+    T_Certification,
+    T_Quest_Certification,
+    T_Programming_Language,
+    T_Topic_Skill,
+    T_Xp_Threshold,
+    Codingame_API_Exeption_Function,
+    Codingame_API_Exeption_Type
+}
