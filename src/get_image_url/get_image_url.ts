d5b55cc93f4541cbfebc22882bbc6ed309f40217
Elnop <elnop@dev.fr>
2024-02-09 09:06:26 +0100

    add get_image_url

---
+++ src/get_image_url/get_image_url.ts
@@ -0,0 +1,3 @@
+export function get_image_url(id: string): string {
+  return "https://static.codingame.com/servlet/fileservlet?id="+id;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { get_codingamer_by_url } from "./get_codingamer_by_url/get_codingamer_by_url";
 import { Codingamer } from './Codingamer/Codingamer';
 import { Codingame_API_Exeption } from './Codingamer_Exeption';
+import { get_image_url } from './get_image_url/get_image_url';
 
 export {
     Codingamer,
     get_codingamer_by_url,
-    Codingame_API_Exeption
+    Codingame_API_Exeption,
+    get_image_url
 }
